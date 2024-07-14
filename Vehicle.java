0123a3335391f97fe3bbabf5e354b9ac07803c3b
A AZIZ CHOUDHURY <aziz2010ac@gmail.com>
2024-07-14 15:48:02 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+package com.example.beans;
+
+public class Vehicle {
+
+    private String name;
+    private String names;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
